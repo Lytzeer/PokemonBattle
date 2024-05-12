e152ad3779f81a473c16cd89b3feb9ee4e690518
Lytzeer <lukasportier@gmail.com>
2024-05-12 18:41:32 +0200

    Pics Data

---
+++ src/data/PokePics.ts
@@ -0,0 +1,409 @@
+type ImagePoke = {
+    id: number,
+    url: string,
+}
+
+const pokeImages:ImagePoke[] = [
+    {
+        id: 1,
+        url: require('../assets/Pokemon/1.png')
+    },
+    {
+        id: 2,
+        url: require('../assets/Pokemon/2.png')
+    },
+    {
+        id: 3,
+        url: require('../assets/Pokemon/3.png')
+    },
+    {
+        id: 4,
+        url: require('../assets/Pokemon/4.png')
+    },
+    {
+        id: 5,
+        url: require('../assets/Pokemon/5.png')
+    },
+    {
+        id: 6,
+        url: require('../assets/Pokemon/6.png')
+    },
+    {
+        id: 7,
+        url: require('../assets/Pokemon/7.png')
+    },
+    {
+        id: 8,
+        url: require('../assets/Pokemon/8.png')
+    },
+    {
+        id: 9,
+        url: require('../assets/Pokemon/9.png')
+    },
+    {
+        id: 10,
+        url: require('../assets/Pokemon/10.png')
+    },
+    {
+        id: 11,
+        url: require('../assets/Pokemon/11.png')
+    },
+    {
+        id: 12,
+        url: require('../assets/Pokemon/12.png')
+    },
+    {
+        id: 13,
+        url: require('../assets/Pokemon/13.png')
+    },
+    {
+        id: 14,
+        url: require('../assets/Pokemon/14.png')
+    },
+    {
+        id: 15,
+        url: require('../assets/Pokemon/15.png')
+    },
+    {
+        id: 16,
+        url: require('../assets/Pokemon/16.png')
+    },
+    {
+        id: 17,
+        url: require('../assets/Pokemon/17.png')
+    },
+    {
+        id: 18,
+        url: require('../assets/Pokemon/18.png')
+    },
+    {
+        id: 19,
+        url: require('../assets/Pokemon/19.png')
+    },
+    {
+        id: 20,
+        url: require('../assets/Pokemon/20.png')
+    },
+    {
+        id: 21,
+        url: require('../assets/Pokemon/21.png')
+    },
+    {
+        id: 22,
+        url: require('../assets/Pokemon/22.png')
+    },
+    {
+        id: 23,
+        url: require('../assets/Pokemon/23.png')
+    },
+    {
+        id: 24,
+        url: require('../assets/Pokemon/24.png')
+    },
+    {
+        id: 25,
+        url: require('../assets/Pokemon/25.png')
+    },
+    {
+        id: 26,
+        url: require('../assets/Pokemon/26.png')
+    },
+    {
+        id: 27,
+        url: require('../assets/Pokemon/27.png')
+    },
+    {
+        id: 28,
+        url: require('../assets/Pokemon/28.png')
+    },
+    {
+        id: 29,
+        url: require('../assets/Pokemon/29.png')
+    },
+    {
+        id: 30,
+        url: require('../assets/Pokemon/30.png')
+    },
+    {
+        id: 31,
+        url: require('../assets/Pokemon/31.png')
+    },
+    {
+        id: 32,
+        url: require('../assets/Pokemon/32.png')
+    },
+    {
+        id: 33,
+        url: require('../assets/Pokemon/33.png')
+    },
+    {
+        id: 34,
+        url: require('../assets/Pokemon/34.png')
+    },
+    {
+        id: 35,
+        url: require('../assets/Pokemon/35.png')
+    },
+    {
+        id: 36,
+        url: require('../assets/Pokemon/36.png')
+    },
+    {
+        id: 37,
+        url: require('../assets/Pokemon/37.png')
+    },
+    {
+        id: 38,
+        url: require('../assets/Pokemon/38.png')
+    },
+    {
+        id: 39,
+        url: require('../assets/Pokemon/39.png')
+    },
+    {
+        id: 40,
+        url: require('../assets/Pokemon/40.png')
+    },
+    {
+        id: 41,
+        url: require('../assets/Pokemon/41.png')
+    },
+    {
+        id: 42,
+        url: require('../assets/Pokemon/42.png')
+    },
+    {
+        id: 43,
+        url: require('../assets/Pokemon/43.png')
+    },
+    {
+        id: 44,
+        url: require('../assets/Pokemon/44.png')
+    },
+    {
+        id: 45,
+        url: require('../assets/Pokemon/45.png')
+    },
+    {
+        id: 46,
+        url: require('../assets/Pokemon/46.png')
+    },
+    {
+        id: 47,
+        url: require('../assets/Pokemon/47.png')
+    },
+    {
+        id: 48,
+        url: require('../assets/Pokemon/48.png')
+    },
+    {
+        id: 49,
+        url: require('../assets/Pokemon/49.png')
+    },
+    {
+        id: 50,
+        url: require('../assets/Pokemon/50.png')
+    },
+    {
+        id: 51,
+        url: require('../assets/Pokemon/51.png')
+    },
+    {
+        id: 52,
+        url: require('../assets/Pokemon/52.png')
+    },
+    {
+        id: 53,
+        url: require('../assets/Pokemon/53.png')
+    },
+    {
+        id: 54,
+        url: require('../assets/Pokemon/54.png')
+    },
+    {
+        id: 55,
+        url: require('../assets/Pokemon/55.png')
+    },
+    {
+        id: 56,
+        url: require('../assets/Pokemon/56.png')
+    },
+    {
+        id: 57,
+        url: require('../assets/Pokemon/57.png')
+    },
+    {
+        id: 58,
+        url: require('../assets/Pokemon/58.png')
+    },
+    {
+        id: 59,
+        url: require('../assets/Pokemon/59.png')
+    },
+    {
+        id: 60,
+        url: require('../assets/Pokemon/60.png')
+    },
+    {
+        id: 61,
+        url: require('../assets/Pokemon/61.png')
+    },
+    {
+        id: 62,
+        url: require('../assets/Pokemon/62.png')
+    },
+    {
+        id: 63,
+        url: require('../assets/Pokemon/63.png')
+    },
+    {
+        id: 64,
+        url: require('../assets/Pokemon/64.png')
+    },
+    {
+        id: 65,
+        url: require('../assets/Pokemon/65.png')
+    },
+    {
+        id: 66,
+        url: require('../assets/Pokemon/66.png')
+    },
+    {
+        id: 67,
+        url: require('../assets/Pokemon/67.png')
+    },
+    {
+        id: 68,
+        url: require('../assets/Pokemon/68.png')
+    },
+    {
+        id: 69,
+        url: require('../assets/Pokemon/69.png')
+    },
+    {
+        id: 70,
+        url: require('../assets/Pokemon/70.png')
+    },
+    {
+        id: 71,
+        url: require('../assets/Pokemon/71.png')
+    },
+    {
+        id: 72,
+        url: require('../assets/Pokemon/72.png')
+    },
+    {
+        id: 73,
+        url: require('../assets/Pokemon/73.png')
+    },
+    {
+        id: 74,
+        url: require('../assets/Pokemon/74.png')
+    },
+    {
+        id: 75,
+        url: require('../assets/Pokemon/75.png')
+    },
+    {
+        id: 76,
+        url: require('../assets/Pokemon/76.png')
+    },
+    {
+        id: 77,
+        url: require('../assets/Pokemon/77.png')
+    },
+    {
+        id: 78,
+        url: require('../assets/Pokemon/78.png')
+    },
+    {
+        id: 79,
+        url: require('../assets/Pokemon/79.png')
+    },
+    {
+        id: 80,
+        url: require('../assets/Pokemon/80.png')
+    },
+    {
+        id: 81,
+        url: require('../assets/Pokemon/81.png')
+    },
+    {
+        id: 82,
+        url: require('../assets/Pokemon/82.png')
+    },
+    {
+        id: 83,
+        url: require('../assets/Pokemon/83.png')
+    },
+    {
+        id: 84,
+        url: require('../assets/Pokemon/84.png')
+    },
+    {
+        id: 85,
+        url: require('../assets/Pokemon/85.png')
+    },
+    {
+        id: 86,
+        url: require('../assets/Pokemon/86.png')
+    },
+    {
+        id: 87,
+        url: require('../assets/Pokemon/87.png')
+    },
+    {
+        id: 88,
+        url: require('../assets/Pokemon/88.png')
+    },
+    {
+        id: 89,
+        url: require('../assets/Pokemon/89.png')
+    },
+    {
+        id: 90,
+        url: require('../assets/Pokemon/90.png')
+    },
+    {
+        id: 91,
+        url: require('../assets/Pokemon/91.png')
+    },
+    {
+        id: 92,
+        url: require('../assets/Pokemon/92.png')
+    },
+    {
+        id: 93,
+        url: require('../assets/Pokemon/93.png')
+    },
+    {
+        id: 94,
+        url: require('../assets/Pokemon/94.png')
+    },
+    {
+        id: 95,
+        url: require('../assets/Pokemon/95.png')
+    },
+    {
+        id: 96,
+        url: require('../assets/Pokemon/96.png')
+    },
+    {
+        id: 97,
+        url: require('../assets/Pokemon/97.png')
+    },
+    {
+        id: 98,
+        url: require('../assets/Pokemon/98.png')
+    },
+    {
+        id: 99,
+        url: require('../assets/Pokemon/99.png')
+    },
+    {
+        id: 100,
+        url: require('../assets/Pokemon/100.png')
+    }
+]
+
+export {pokeImages, ImagePoke}
---
+++ src/data/TypePics.ts
@@ -0,0 +1,81 @@
+type ImageType = {
+    id: number,
+    url: string,
+}
+
+const imagesTypes: ImageType[] = [
+    {
+        id: 1,
+        url: require('../assets/Type/1.png')
+    },
+    {
+        id: 2,
+        url: require('../assets/Type/2.png')
+    },
+    {
+        id: 3,
+        url: require('../assets/Type/3.png')
+    },
+    {
+        id: 4,
+        url: require('../assets/Type/4.png')
+    },
+    {
+        id: 5,
+        url: require('../assets/Type/5.png')
+    },
+    {
+        id: 6,
+        url: require('../assets/Type/6.png')
+    },
+    {
+        id: 7,
+        url: require('../assets/Type/7.png')
+    },
+    {
+        id: 8,
+        url: require('../assets/Type/8.png')
+    },
+    {
+        id: 9,
+        url: require('../assets/Type/9.png')
+    },
+    {
+        id: 10,
+        url: require('../assets/Type/10.png')
+    },
+    {
+        id: 11,
+        url: require('../assets/Type/11.png')
+    },
+    {
+        id: 12,
+        url: require('../assets/Type/12.png')
+    },
+    {
+        id: 13,
+        url: require('../assets/Type/13.png')
+    },
+    {
+        id: 14,
+        url: require('../assets/Type/14.png')
+    },
+    {
+        id: 15,
+        url: require('../assets/Type/15.png')
+    },
+    {
+        id: 16,
+        url: require('../assets/Type/16.png')
+    },
+    {
+        id: 17,
+        url: require('../assets/Type/17.png')
+    },
+    {
+        id: 18,
+        url: require('../assets/Type/18.png')
+    }
+]
+
+export { imagesTypes, ImageType }
